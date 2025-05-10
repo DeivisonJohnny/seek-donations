0fb3c66c09a2a68c8cc737e836f2ecb0773cf759
Deivison Johnnny <deivisonjohnny@gmail.com>
2025-05-10 18:17:47 -0300

    feat: create models primary

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,14 +1,17 @@
 import { ReactNode } from "react";
 import Header from "./Header";
+import { usePathname } from "next/navigation";
 
 type Props = {
   children: ReactNode;
 };
 
 export default function Layout({ children }: Props) {
+  const router = usePathname();
+
   return (
     <>
-      <Header />
+      {router != "/" ? <Header /> : null}
       {children}
     </>
   );
